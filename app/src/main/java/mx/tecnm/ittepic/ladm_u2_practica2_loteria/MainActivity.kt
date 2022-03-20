f89be15ec5fa88f6dde302bfc39c70302392f83d
Carlos Uriel Fregoso Espericueta <caurfregosoes@ittepic.edu.mx>
2022-03-20 13:22:33 -0700

    project creation

---
+++ app/src/main/java/mx/tecnm/ittepic/ladm_u2_practica2_loteria/MainActivity.kt
@@ -0,0 +1,11 @@
+package mx.tecnm.ittepic.ladm_u2_practica2_loteria
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
