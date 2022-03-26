e6401fbfcb8dc8c8b9066f0fe4dfaca0ea3dacbf
Carlos Uriel Fregoso Espericueta <caurfregosoes@ittepic.edu.mx>
2022-03-25 22:28:17 -0700

    Add for more than 1 game, solved some logic problems on buttons App v 1.3.0

--- app/src/main/java/mx/tecnm/ittepic/ladm_u2_practica2_loteria/MainActivity.kt
+++ app/src/main/java/mx/tecnm/ittepic/ladm_u2_practica2_loteria/MainActivity.kt
@@ -1,15 +1,22 @@
 package mx.tecnm.ittepic.ladm_u2_practica2_loteria
 
+import android.content.Intent
 import android.media.MediaPlayer
-import androidx.appcompat.app.AppCompatActivity
+import android.os.Build
 import android.os.Bundle
 import android.view.View
 import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
+import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
-import kotlinx.coroutines.*
+import kotlinx.coroutines.CoroutineStart
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 import mx.tecnm.ittepic.ladm_u2_practica2_loteria.databinding.ActivityMainBinding
 import kotlin.coroutines.EmptyCoroutineContext
+
 
 class MainActivity : AppCompatActivity() {
     lateinit var binding: ActivityMainBinding
@@ -26,7 +33,10 @@ class MainActivity : AppCompatActivity() {
     lateinit var mediaCo: MediaPlayer
     var mediaBack: MediaPlayer ?= MediaPlayer()
 
-    private val scope = GlobalScope.launch(EmptyCoroutineContext,CoroutineStart.LAZY) {
+    // ------------------------------------------------------- fin de la declaracion de las variables globales
+    // ------------------------------------------------------- Inicio de la decalraacin de las corrutinas
+
+    private val initBackmedia = GlobalScope.launch(EmptyCoroutineContext,CoroutineStart.LAZY) {
        delay(500)
         while(true) {
             mediaCo = MediaPlayer.create(baseContext, R.raw.ascensor3)
@@ -35,7 +45,7 @@ class MainActivity : AppCompatActivity() {
             mediaCo.stop()
         }
     }
-    private val musicScope = GlobalScope.launch(EmptyCoroutineContext,CoroutineStart.LAZY) {
+    private val whileMusicMedia = GlobalScope.launch(EmptyCoroutineContext,CoroutineStart.LAZY) {
         while(true){
             mediaBack = MediaPlayer.create(baseContext,R.raw.fondopiano)
             mediaBack!!.start()
@@ -59,37 +69,44 @@ class MainActivity : AppCompatActivity() {
             recycler.adapter = adapterMain
         }
     }
-
+// ----------------------------------------------------------------- fin de la declaracion de las corrtinas
+// ----------------------------------------------------------------- inicio de los metodos de manipulacion
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        var restantes = binding.btnCartasRstantes
-        restantes.visibility = View.INVISIBLE
+        AlertDialog.Builder(this)
+            .setTitle("Aviso!")
+            .setMessage("Tiene musica de fondo, tarda un poco en iniciar." +
+                "\nAdemás hay un intervalo de 9 segundos en las cartas, esto ya que algunas frases estan muy largas")
+            .setNeutralButton("nice",{d,i ->{}})
+            .show()
 
-        var buenas = binding.btnBuenas
+        val restantes = binding.btnCartasRstantes
+        val buenas = binding.btnBuenas
+        val inicio = binding.btnInicar
+        val barajasB = binding.btnBarajear
+        val floating = binding.floatinButton
+
         buenas.visibility = View.INVISIBLE
-
-        var inicio = binding.btnInicar
+        restantes.visibility = View.INVISIBLE
         inicio.visibility = View.INVISIBLE
-
-        var btnbarajas = binding.btnBarajear
-
+        floating.visibility = View.INVISIBLE
 
         barajas = Barajeo(this,vectorBarajeo)
         binding.imgCartas.setImageResource(R.drawable.esperanding)
 
-        scope.start()
+        initBackmedia.start()
 
         //----------------------------------------------------------------- Sección de los botones
         binding.btnBarajear.setOnClickListener {
             vectorBarajeo.shuffle()
-            scope.cancel()
+            initBackmedia.cancel()
             Toast.makeText(this, "Mazo barajeado!!", Toast.LENGTH_SHORT).show()
             inicio.visibility = View.VISIBLE
             mediaCo.release()
-            musicScope.start()
+            whileMusicMedia.start()
         }
 
         binding.btnInicar.setOnClickListener {
@@ -97,32 +114,59 @@ class MainActivity : AppCompatActivity() {
             binding.imgCartas.setImageResource(R.drawable.esperanding)
             binding.txtVTituloCartas.text = "Esperanding....."
             buenas.visibility = View.VISIBLE
+            barajasB.visibility = View.INVISIBLE
+            inicio.visibility = View.INVISIBLE
+
         } // fin del primer boton, el de inicio
 
         binding.btnBuenas.setOnClickListener {
-            if(barajas.estaPausado()){
-                barajas.despausaBarajeo()
-                Toast.makeText(this, "Espera a que se termine de decir la carta", Toast.LENGTH_SHORT).show()
+            if(!barajas.estaPausado()){
+                binding.btnBuenas.text = "No buenas :("
+                barajas.pausaBarajeo()
+                Toast.makeText(this, "Presiona de nuevo para despausar", Toast.LENGTH_SHORT).show()
+                restantes.visibility = View.VISIBLE
+                barajasB.visibility = View.INVISIBLE
+                inicio.visibility = View.INVISIBLE
                 return@setOnClickListener
             }
-            barajas.pausaBarajeo()
-            restantes.visibility = View.VISIBLE
-            btnbarajas.visibility = View.INVISIBLE
-            inicio.visibility = View.INVISIBLE
+
+            buenas.text = "Buenas!!"
+            barajas.despausaBarajeo()
+            restantes.visibility = View.INVISIBLE
+            barajasB.visibility = View.VISIBLE
+            inicio.visibility = View.VISIBLE
             victorySound.start()
-            if (victorySound.isCompleted){victorySound.cancel()}
-            Toast.makeText(this, "Presiona de nuevo para despausar", Toast.LENGTH_SHORT).show()
+            if (victorySound.isCompleted){
+                victorySound.cancel()
+            }
+
         }// fin del set on click para buenas
 
         binding.btnCartasRstantes.setOnClickListener {
             if(barajas.estaEjecutandose()){
                 cardView.start()
-                musicScope.cancel()
+                whileMusicMedia.cancel()
                 mediaBack!!.release()
                 barajas.terminarBarajeo()
+                buenas.visibility = View.INVISIBLE
+                floating.visibility = View.VISIBLE
+                Toast.makeText(this, "Si quiere volver a jugar, reinicie el juego con el boton de abajo", Toast.LENGTH_LONG).show()
             }
             Toast.makeText(this, "Juego terminado", Toast.LENGTH_SHORT).show()
             return@setOnClickListener
+        }
+
+        floating.setOnClickListener {
+            if (Build.VERSION.SDK_INT >= 11) {
+                recreate()
+            } else {
+                val intent = intent
+                intent.addFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION)
+                finish()
+                overridePendingTransition(0, 0)
+                startActivity(intent)
+                overridePendingTransition(0, 0)
+            }
         }
 
     }// fin del OnCreate
